caccba946cf1a86ba7579511947ec0323d5f4707
Aviv Liberman <avivli@wix.com>
2022-02-10 16:34:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,8 @@ class Symbol {
   }
   draw(context) {
     this.text = this.characters.charAt(Math.floor(Math.random()*this.characters.length));
-    context.fillStyle = '#0aff0a';
     context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-    if (this.y * this.fontSize > this.canvasHeight) {
+    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
       this.y = 0;
     } else {
       this.y += 1;
@@ -38,16 +37,44 @@ class Effect {
       this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
     }
   }
+
+  resize(width, hieght){
+    this.canvasWidth = width;
+    this.canvasHeight = hieght;
+    this.colums = this.canvasWidth/this.fontSize;
+    this.symbols = [];
+    this.#initialize();
+  }
 }
 
 const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 30;
+const nextFrame = 1000/fps;
+let timer = 0;
 
-function animate() {
-  ctx.fillStyle = 'rgba(0 ,0 ,0 , 0.05)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.font = effect.fontSize + 'px monospace';
-  effect.symbols.forEach(symbol => symbol.draw(ctx));
+function animate(timestamp) {
+  const deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+  if(timer > nextFrame) {
+    ctx.fillStyle = 'rgba(0 ,0 ,0 , 0.05)';
+    ctx.textAlign = 'center';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#0aff0a';
+    ctx.font = effect.fontSize + 'px monospace';
+    effect.symbols.forEach(symbol => symbol.draw(ctx));
+    timer = 0;
+  } else {
+    timer += deltaTime;
+  }
+
   requestAnimationFrame(animate);
 }
 
-animate();
+animate(0);
+
+window.addEventListener('resize', function(){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  effect.resize(canvas.width, canvas.height)
+});
